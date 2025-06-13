dcaeaa1bfee84791493020866368394dccc7e702
timdohring <84195501+timdohring@users.noreply.github.com>
2025-06-13 14:28:14 -0400

    testing and debugging get_block

--- src/generator/terrain/tree_cutter.rs
+++ src/generator/terrain/tree_cutter.rs
@@ -38,7 +38,7 @@ pub async fn log_trees(editor: &mut Editor, points: HashSet<Point2D>) {
         let mut point3d = Point3D::new(point.x, height, point.y);
         let mut block_id = editor.get_block(point3d).id;
 
-        if !block_id.is_tree_or_leaf() {
+        if !block_id.is_leaf() {
             //println!("Skipping block {:?} at point {:?}: block is not a tree or leaf", block_id, point3d);
             editor.place_block(&Block::new(BlockID::BlueWool, None, None), point3d).await;
             continue;
@@ -47,7 +47,7 @@ pub async fn log_trees(editor: &mut Editor, points: HashSet<Point2D>) {
         editor.place_block(&Block::new(BlockID::RedWool, None, None), point3d).await;
         for y in 1..40 {
             block_id = editor.get_block(Point3D::new(point.x, height - y, point.y)).id;
-            if block_id.is_tree_or_leaf() {
+            if block_id.is_leaf() {
                 editor.place_block(&Block::new(BlockID::BlackWool, None, None), Point3D::new(point.x, height - y, point.y)).await;
             } else if block_id == BlockID::Dirt {
                 editor.place_block(&Block::new(BlockID::GreenWool, None, None), Point3D::new(point.x, height - y, point.y)).await;
