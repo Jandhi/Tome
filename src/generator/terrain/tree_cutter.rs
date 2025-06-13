f89da3f446045e287b390d36df33e19cdb3c377b
timdohring <84195501+timdohring@users.noreply.github.com>
2025-06-13 14:53:02 -0400

    logger working

--- src/generator/terrain/tree_cutter.rs
+++ src/generator/terrain/tree_cutter.rs
@@ -38,19 +38,16 @@ pub async fn log_trees(editor: &mut Editor, points: HashSet<Point2D>) {
         let mut point3d = Point3D::new(point.x, height, point.y);
         let mut block_id = editor.get_block(point3d).id;
 
-        if !block_id.is_leaf() {
-            //println!("Skipping block {:?} at point {:?}: block is not a tree or leaf", block_id, point3d);
-            editor.place_block(&Block::new(BlockID::BlueWool, None, None), point3d).await;
+        if !block_id.is_tree_or_leaf() {
             continue;
         }
-        //println!("Logging tree at point {:?} with block {:?}", point3d, block_id);
-        editor.place_block(&Block::new(BlockID::RedWool, None, None), point3d).await;
+        editor.place_block(&Block::new(BlockID::Air, None, None), point3d).await;
         for y in 1..40 {
             block_id = editor.get_block(Point3D::new(point.x, height - y, point.y)).id;
-            if block_id.is_leaf() {
-                editor.place_block(&Block::new(BlockID::BlackWool, None, None), Point3D::new(point.x, height - y, point.y)).await;
+            if block_id.is_tree_or_leaf() {
+                editor.place_block(&Block::new(BlockID::Air, None, None), Point3D::new(point.x, height - y, point.y)).await;
             } else if block_id == BlockID::Dirt {
-                editor.place_block(&Block::new(BlockID::GreenWool, None, None), Point3D::new(point.x, height - y, point.y)).await;
+                editor.place_block(&Block::new(BlockID::GrassBlock, None, None), Point3D::new(point.x, height - y, point.y)).await;
             } else if block_id != BlockID::Air {
                 continue;
             }
