9cfb678e976b66549e208cdbcb3dec46a9a16616
timdohring <84195501+timdohring@users.noreply.github.com>
2025-06-13 22:05:29 -0400

    wall progress

--- src/generator/districts/wall.rs
+++ src/generator/districts/wall.rs
@@ -1,13 +1,13 @@
 use std::collections::{HashMap, HashSet};
 use log::info;
 
-use crate::{editor::World, generator::districts::wall, geometry::{get_neighbours_in_set, get_outer_points, Point2D, Point3D}};
+use crate::{editor::World, generator::{BuildClaim, materials::MaterialPlacer}, noise::RNG, geometry::{get_neighbours_in_set, get_outer_points, Point2D, Point3D,}};
 
 use crate::editor::Editor;
 
 fn get_wall_points(
     inner_points: &HashSet<Point2D>,
-    editor: &Editor,
+    editor: &mut Editor,
 ) -> (HashSet<Point2D>) {
     let mut wall_points = get_outer_points(inner_points);
 
@@ -15,9 +15,10 @@ fn get_wall_points(
     let mut to_remove = Vec::new();
 
     for point in &wall_points {
-        let neighbours = get_neighbours_in_set(point, inner_points);
+        editor.world().claim(*point, BuildClaim::Wall); // mark wall points as claimed
+        let neighbours = get_neighbours_in_set(*point, inner_points);
         if neighbours.len() == 1 { // supposed to remove extra points
-            to_remove.push(point.clone());
+            to_remove.push(*point);
         }
     }
 
@@ -28,7 +29,7 @@ fn get_wall_points(
     wall_points
 }
 
-fn find_wall_neighbour(point: &Point2D, wall_points: &HashSet<Point2D>, ordered_set: &HashSet<Point2D>) -> Option<Point2D> {
+fn find_wall_neighbour(point: Point2D, wall_points: &HashSet<Point2D>, ordered_set: &HashSet<Point2D>) -> Option<Point2D> {
     // checking neighbours in a specific order to ensure consistent ordering
     let directions = [
         Point2D { x: -1, y: 0 },
@@ -43,7 +44,7 @@ fn find_wall_neighbour(point: &Point2D, wall_points: &HashSet<Point2D>, ordered_
 
     // Check all neighbours of the point in the wall points
     for direction in directions.iter() {
-        let neighbour = *point + *direction;
+        let neighbour = point + *direction;
         if !ordered_set.contains(&neighbour) && wall_points.contains(&neighbour) {
             return Some(neighbour);
         }
@@ -52,22 +53,22 @@ fn find_wall_neighbour(point: &Point2D, wall_points: &HashSet<Point2D>, ordered_
 }
 
 pub fn order_wall_points(
-    wall_points: & mut HashSet<Point2D>,
+    wall_points: & HashSet<Point2D>,
 ) -> Vec<Vec<Point2D>> {
     let mut list_of_ordered_vec = Vec::new();
 
-    let mut wall_point_list = wall_points.iter().collect::<Vec<_>>();
+    let mut wall_point_list = wall_points.iter().cloned().collect::<Vec<_>>();
     let mut ordered_vec = Vec::new();
     let mut ordered_set = HashSet::new();
     let mut current_point = wall_point_list.remove(0);
 
-    ordered_vec.push(*current_point);
-    ordered_set.insert(*current_point);
+    ordered_vec.push(current_point);
+    ordered_set.insert(current_point);
 
     let mut reverse_check = false;
 
     while wall_point_list.len() > 0 {
-        let next_wall_point = find_wall_neighbour(&current_point, wall_points, &ordered_set);
+        let next_wall_point = find_wall_neighbour(current_point, wall_points, &ordered_set);
         if next_wall_point.is_none() {
             // If no next point is found, we need to reverse the direction
             if reverse_check {
@@ -79,26 +80,35 @@ pub fn order_wall_points(
                 }
                 ordered_vec.clear();
                 current_point = wall_point_list.remove(0);
-                ordered_vec.push(*current_point);
-                ordered_set.insert(*current_point);
+                ordered_vec.push(current_point);
+                ordered_set.insert(current_point);
                 break; // If we already reversed, we are done
             } else {
                 info!("Reversing wall");
                 reverse_check = true;
                 // Reverse the order of the ordered_vec
                 ordered_vec.reverse();
-                current_point = &ordered_vec.first().cloned().unwrap();
+                current_point = ordered_vec.first().cloned().unwrap();
                 continue;
             }
         
         } else {
-            wall_point_list.retain(|&p| p != next_wall_point.unwrap());
-            ordered_vec.push(*current_point);
-            ordered_set.insert(*current_point);
-            current_point = &next_wall_point.unwrap();
+            wall_point_list.retain(|p| *p != next_wall_point.unwrap());
+            ordered_vec.push(current_point);
+            ordered_set.insert(current_point);
+            current_point = next_wall_point.unwrap();
         }
     }
 
     list_of_ordered_vec.push(ordered_vec);
     list_of_ordered_vec
+}
+
+pub async fn build_wall(urban_points: &HashSet<Point2D>, editor: &mut Editor, rng : &RNG, MaterialPlacer: & MaterialPlacer<'_>){
+    let mut wall_points = get_wall_points(urban_points, editor);
+    let ordered_wall_points = order_wall_points(&wall_points);
+
+    for wall_point_list in ordered_wall_points {
+        continue
+    }
 }
--- src/geometry/functions.rs
+++ src/geometry/functions.rs
@@ -2,10 +2,10 @@ use std::collections::{HashMap, HashSet};
 
 use crate::{geometry::{Point2D, Point3D, CARDINALS_2D}};
 
-pub fn get_neighbours_in_set(point: &Point2D, points: &HashSet<Point2D>) -> Vec<Point2D> {
+pub fn get_neighbours_in_set(point: Point2D, points: &HashSet<Point2D>) -> Vec<Point2D> {
     let mut neighbours = Vec::new();
     for cardinal in CARDINALS_2D {
-        let neighbour = *point + cardinal;
+        let neighbour = point + cardinal;
         if points.contains(&neighbour) {
             neighbours.push(neighbour);
         }
@@ -13,10 +13,10 @@ pub fn get_neighbours_in_set(point: &Point2D, points: &HashSet<Point2D>) -> Vec<
     neighbours
 }
 
-pub fn get_neighbours_not_in_set(point: &Point2D, points: &HashSet<Point2D>) -> Vec<Point2D> {
+pub fn get_neighbours_not_in_set(point: Point2D, points: &HashSet<Point2D>) -> Vec<Point2D> {
     let mut neighbours = Vec::new();
     for cardinal in CARDINALS_2D {
-        let neighbour = *point + cardinal;
+        let neighbour = point + cardinal;
         if !points.contains(&neighbour) {
             neighbours.push(neighbour);
         }
@@ -24,26 +24,22 @@ pub fn get_neighbours_not_in_set(point: &Point2D, points: &HashSet<Point2D>) -> 
     neighbours
 }
 
-
-pub fn get_outer_points(points: &HashSet<Point2D>) -> (HashSet<Point2D>) {
+pub fn get_outer_points(points: &HashSet<Point2D>) -> HashSet<Point2D> {
     let mut outer_points = HashSet::new();
 
     for point in points {
-        let neighbours = get_neighbours_in_set(point, points);
+        let neighbours = get_neighbours_in_set(*point, points);
         if neighbours.len() < 4 {
-            outer_points.insert(point.clone());
+            outer_points.insert(*point);
         }
     }
     outer_points
 }
 
-
-pub fn get_outer_and_inner_points(
-    points: &HashSet<Point2D>, distance: u32
-) -> (HashSet<Point2D>, HashSet<Point2D>) {
+pub fn get_outer_and_inner_points(points: &HashSet<Point2D>, distance: u32) -> (HashSet<Point2D>, HashSet<Point2D>) {
     let mut outer_points = get_outer_points(points);
     let mut visited = outer_points.clone();
-    let mut queue = outer_points.iter().map(|p| (p.clone(), 0 as u32)).collect::<Vec<_>>();
+    let mut queue = outer_points.iter().map(|p| (*p, 0 as u32)).collect::<Vec<_>>();
 
     while queue.len() > 0 {
         let (point, edge_distance) = queue.remove(0);
@@ -59,31 +55,31 @@ pub fn get_outer_and_inner_points(
             if visited.contains(&neighbour) {
                 continue;
             }
-            visited.insert(neighbour.clone());
-            outer_points.insert(neighbour.clone());
+            visited.insert(neighbour);
+            outer_points.insert(neighbour);
             queue.push((neighbour, edge_distance + 1));
         }
     }
 
-    let inner_points = points - &outer_points;
+    let inner_points: HashSet<Point2D> = points.difference(&outer_points).cloned().collect();
 
     (outer_points, inner_points)
 }
 
-pub fn is_straight_point2d(first: &Point2D, second: &Point2D, length: i32) -> bool {
-    let line = *first - *second;
-    return ((line.x.abs() == length || line.y.abs() == length) && (line.x == 0 || line.y == 0)) || 
-            (line.x.abs() == length && line.y.abs() == length)
+pub fn is_straight_point2d(first: Point2D, second: Point2D, length: i32) -> bool {
+    let line = first - second;
+    ((line.x.abs() == length || line.y.abs() == length) && (line.x == 0 || line.y == 0)) || 
+        (line.x.abs() == length && line.y.abs() == length)
 }
 
-pub fn is_straight_not_diagonal_point2d(first: &Point2D, second: &Point2D, length: i32) -> bool {
-    let line = *first - *second;
-    return (line.x.abs() == length || line.y.abs() == length) && (line.x == 0 || line.y == 0)
+pub fn is_straight_not_diagonal_point2d(first: Point2D, second: Point2D, length: i32) -> bool {
+    let line = first - second;
+    (line.x.abs() == length || line.y.abs() == length) && (line.x == 0 || line.y == 0)
 }
 
 // include diagonal points
 pub fn is_point_surrounded_by_points(
-    point: &Point2D, points: &HashSet<Point2D>
+    point: Point2D, points: &HashSet<Point2D>
 ) -> bool {
     // Define all 8 directions (cardinals + diagonals)
     let directions = [
@@ -98,7 +94,7 @@ pub fn is_point_surrounded_by_points(
     ];
 
     for direction in directions.iter() {
-        let neighbour = *point + *direction;
+        let neighbour = point + *direction;
         if !points.contains(&neighbour) {
             return false;
         }
@@ -123,6 +119,6 @@ pub fn get_surrounding_set(points: &HashSet<Point2D>, distance: u32) -> HashSet<
     if distance == 1 {
         return surrounding;
     } else {
-        return surrounding.union(&get_surrounding_set(&surrounding, distance - 1)).cloned().collect();
+        return surrounding.union(&get_surrounding_set(points, distance - 1)).cloned().collect();
     }
 }
