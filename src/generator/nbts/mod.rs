43d2a39bc6db5eed76537b573b76b8fa22a0aa03
Jan D <jandohring@hotmail.com>
2025-06-13 21:13:25 -0400

    empty

--- src/generator/nbts/mod.rs
+++ src/generator/nbts/mod.rs
@@ -7,9 +7,7 @@ mod meta;
 mod structure;
 
 pub use place::{place_nbt, place_nbt_without_palette};
-pub use structure::Structure;
-pub use transform::{Transform, Rotation};
-pub use meta::NBTMeta;
-pub use transform::Transform;
-pub use rotation::Rotation;
 pub use structure::{Structure, StructureId};
+pub use transform::Transform;
+pub use meta::NBTMeta;
+pub use rotation::Rotation;
